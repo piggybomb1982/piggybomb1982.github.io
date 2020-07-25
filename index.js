59e0064972ef684b782151a5962d3ecd40d62719
Yasushi Shiraishi <shiraishi20040707@yahoo.co.jp>
2020-07-25 19:10:28 +0900

    test

--- index.js
+++ index.js
@@ -9,6 +9,10 @@
     var x_old = Array(POSENUM);
     var y_old = Array(POSENUM);
     var r2_old = 0;
+    var totalTime = 0;
+    var startTime = performance.now(); // 開始時間
+    var endTime = performance.now(); // 終了時間
+    var eflag = false; 
 
     main();
 
@@ -47,6 +51,15 @@
             function(err) {
             alert(err.name + ": " + err.message);
         });
+
+        doc.addEventListener("click", () => {
+            if(eflag){
+
+            }
+            else{
+                eflag = true;
+            }
+          }, false);
     }
 
     async function updateImage(video, net){
@@ -64,74 +77,38 @@
         //context.clearRect(0, 0, WSIZE, HSIZE);
         context.drawImage(video,0 , 0, WSIZE, HSIZE, 0,0,WSIZE, HSIZE);
 
-        var cnt = 0;
-        var r2 = 0;
-        for(var i=0; i<POSENUM; i++){
-            if(pose.keypoints[i].score > 0.5){
-                context.fillStyle = "rgb(255, 0, 0)";
-                context.fillRect(pose.keypoints[i].position.x, pose.keypoints[i].position.y, 10, 10);
-                r2 = Math.sqrt((pose.keypoints[i].position.x - x_old[i])^2 + (pose.keypoints[i].position.y - y_old[i])^2 );
-                x_old[i] = pose.keypoints[i].position.x;
-                y_old[i] = pose.keypoints[i].position.y;
-                cnt = cnt + 1;
+        if(eflag){
+            var cnt = 0;
+            var r2 = 0;
+            for(var i=0; i<POSENUM; i++){
+                if(pose.keypoints[i].score > 0.5){
+                    context.fillStyle = "rgb(255, 0, 0)";
+                    context.fillRect(pose.keypoints[i].position.x, pose.keypoints[i].position.y, 10, 10);
+                    r2 = Math.sqrt((pose.keypoints[i].position.x - x_old[i])^2 + (pose.keypoints[i].position.y - y_old[i])^2 );
+                    x_old[i] = pose.keypoints[i].position.x;
+                    y_old[i] = pose.keypoints[i].position.y;
+                    cnt = cnt + 1;
+                }
             }
-        }
-        if(isNaN(r2/cnt)){
-            r2 = r2_old;
-        }
-        else{
-            r2 = r2 / cnt;
-            r2_old = r2;
-        } 
-        //文字のスタイルを指定
-        context.font = '32px serif';
-        context.fillStyle = '#FF0000';
-        //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
-        context.textBaseline = 'bottom';
-        context.textAlign = 'center';
+            if(isNaN(r2/cnt)){
+                r2 = r2_old;
+            }
+            else{
+                r2 = r2 / cnt;
+                r2_old = r2;
+            } 
+            //文字のスタイルを指定
+            context.font = '32px serif';
+            context.fillStyle = '#FF0000';
+            //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
+            context.textBaseline = 'bottom';
+            context.textAlign = 'center';
 
-        //座標を指定して文字を描く（座標は画像の中心に）
-        context.fillText("現在の活性度 " + String(r2), 0, HSIZE-50);
+            //座標を指定して文字を描く（座標は画像の中心に）
+            context.fillText("現在の活性度 " + String(r2), 0, HSIZE-50);
+        }
 
     }
-
-    //function getDuration() {
-        //動画の長さ（秒）を表示
-        //doc.getElementById("nagasa").innerHTML = CAMERA.duration;
-    //}
-    //function getPose(r2) {
-        //動画の長さ（秒）を表示
-        //doc.getElementById("nagasa").innerHTML = r2;
-    //}
-    //function playVideo() {
-        //再生完了の表示をクリア
-        //doc.getElementById("kanryou").innerHTML = "";
-        //動画を再生
-        //CAMERA.play();
-        //現在の再生位置（秒）を表示
-        //CAMERA.addEventListener("timeupdate", function(){
-        //    doc.getElementById("ichi").innerHTML = CAMERA.currentTime;
-        //}, false);
-        //再生完了を知らせる
-        //CAMERA.addEventListener("ended", function(){
-        //    doc.getElementById("kanryou").innerHTML = "動画の再生が完了しました。";
-        //}, false);
-    //}
-
-    //function pauseVideo() {
-        //動画を一時停止
-    //    CAMERA.pause();
-    //}
-
-    //function upVolume() {
-        //音量を上げる
-    //    CAMERA.volume = CAMERA.volume + 0.25;
-    //}
-
-    //function downVolume() {
-        //音量を下げる
-    //    CAMERA.volume = CAMERA.volume - 0.25;
-    //}
 
 
 })(this, document);
